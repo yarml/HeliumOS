303524e572bee2017ba9a6a18244f5cd8edac995
Yava <yava.coco@gmail.com>
2022-08-05 18:32:42 +0100

    Add mutex lock() and unlock()
    Copied the code from osdev =]

---
+++ kernel/include/mutex.h
@@ -0,0 +1,9 @@
+#ifndef HELIUM_MUTEX_H
+#define HELIUM_MUTEX_H
+
+typedef volatile int mutex;
+
+void mutex_lock(mutex*);
+void mutex_ulock(mutex*);
+
+#endif
---
+++ kernel/src/mutex.c
@@ -0,0 +1,14 @@
+#include <mutex.h>
+#include <sys.h>
+
+void mutex_lock(mutex* mutex)
+{
+    while(!__sync_bool_compare_and_swap(mutex, 0, 1))
+        pause();
+}
+
+void mutex_ulock(mutex* mutex)
+{
+    *mutex = 0;
+}
+

169d04723582bd44625d0f09521d935e3c4262da
yarml <youssefharmal@gmail.com>
2023-02-16 12:02:54 +0100

    Return i_pmlmax

--- kernel/src/mem/internal_mem.h
+++ kernel/src/mem/internal_mem.h
@@ -18,10 +18,13 @@
 #define ADR_MASK(n)  (uintptr_t) ((uintptr_t) 0x1FF << (12 + 9 * (n)))
 #define ADR_SHIFT(n) (size_t) (12 + 9 * (n))
 
+// Gives the index of the entry in the parent structure of order n
+// that leads to pointing out to ptr
 #define ENTRY_IDX(n, ptr) \
     (size_t) (((uintptr_t) (ptr) & ADR_MASK(n)) >> ADR_SHIFT(n))
 
 // TODO: when porting to other architectures, this should be considered
+// I'll probably never port, struggling enough with x86-64 lol
 #define ORDER_COUNT (4)
 #define MAX_ORDER   (ORDER_COUNT - 1)
 
@@ -35,6 +38,9 @@ extern size_t i_mmap_usable_len;
 // The physical address of the VMM structure of order MAX_ORDER(PML4)
 // As set by bootboot
 extern mem_vpstruct_ptr *i_ppmlmax;
+// The virtual address of the VMM structure of order MAX_ORDER(PML4)
+// As set by vcache
+extern mem_vpstruct_ptr *i_pmlmax;
 
 extern size_t i_order_ps[ORDER_COUNT];
 

340a2ae50adeea092118f7ca96a86b654d97f287
yarml <youssefharmal@gmail.com>
2023-02-20 19:14:29 +0100

    Simplify error_ functions implementation
    * Make error_general more general :)
    * Add error_inv_state

--- kernel/include/error.h
+++ kernel/include/error.h
@@ -3,7 +3,7 @@
 
 typedef int errno_t;
 
-void error_general(char const *details);
-void error_out_of_memory(char const *details);
+void error_general(char *prefix, char *details);
+void error_out_of_memory(char *details);
 
 #endif
--- kernel/src/error.c
+++ kernel/src/error.c
@@ -3,27 +3,18 @@
 #include <stdio.h>
 #include <sys.h>
 
-void error_general(char const *details)
+void error_general(char *prefix, char *details)
 {
-  fputs(details, stderr);
+  fprintf(stderr, "[%s] %s\n", prefix, details);
   stop();
 }
 
-void error_out_of_memory(char const *details)
+void error_out_of_memory(char *details)
 {
-  char const *prefix = "[Out of memory]";
-  size_t preflen = strlen(prefix);
-  size_t detlen = strlen(details);
-  char buf[preflen + detlen + 2]; // +1 space +1 null termination
-  if(!details)
-  {
-    error_general(prefix);
-    return;
-  }
-  // Concatenating strings in C be like...
-  strcpy(buf, prefix);
-  strcat(buf, " ");
-  strcat(buf, details);
+  error_general("Out of memory", details);
+}
 
-  error_general(buf);
+void error_inv_state(char *details)
+{
+  error_general("Invalid state", details);
 }

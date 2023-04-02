eef69acbddaa2b7ba38432a4b2f2e8a7f060a31c
yarml <youssefharmal@gmail.com>
2023-04-02 11:56:49 +0000

    Fix memcmp

--- kernel/src/std/string/mem/memcmp.c
+++ kernel/src/std/string/mem/memcmp.c
@@ -1,16 +1,18 @@
 #include <stddef.h>
+#include <stdio.h>
 
 int memcmp(void const *b1, void const *b2, size_t size)
 {
   if (b1 == b2)
     return 0;
+
   // TODO: I am too lazy to think about this now, but I am sur there
   // exist some REP instruction to do this faster
-  while (*(unsigned char *)b1 == *(unsigned char *)b2 && size != 0)
+  while (size && *(unsigned char *)b1 == *(unsigned char *)b2)
   {
     --size;
     ++b1;
     ++b2;
   }
-  return *(unsigned char *)(b2 - 1) - *(unsigned char *)(b1 - 1);
+  return *(unsigned char *)(b2) - *(unsigned char *)(b1);
 }

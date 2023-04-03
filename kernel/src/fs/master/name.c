34082266491d2049051705e6240a264bd7520ea5
yarml <youssefharmal@gmail.com>
2023-04-03 11:57:41 +0000

    Implement basic fs path/name verification/parsing functions
    * Implement fs_valid_sys_name to verify that a string is a valid fs name
    * Implement fs_valid_node_name to verify that a string is a valid fs
      file/dir name
    * Implement fs_makecanonical to transform a valid path into a valid
      canonical path
    * Implement fs_pathtok to tokenize a valid path

---
+++ kernel/src/fs/master/name.c
@@ -0,0 +1,201 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <fs.h>
+
+int fs_valid_sys_name(char *name)
+{
+  size_t name_len = strlen(name);
+
+  if(!name_len || name_len >= FS_NAMELEN)
+    return 0;
+
+  // Cannot start or end with '-' or '_'
+  if(!isalnum(name[0]))
+    return 0;
+  if(!isalnum(name[name_len - 1]))
+    return 0;
+
+  for(size_t i = 1; i < name_len - 1; ++i)
+    if(!isalnum(name[i]) && name[i] != '-' && name[i] != '_')
+      return 0;
+  return 1;
+}
+
+int fs_valid_node_name(char *name)
+{
+  size_t name_len = strlen(name);
+
+  if(!name_len || name_len >= FSNODE_NAMELEN)
+    return 0;
+
+  // Cannot being with '-$%#> '
+  switch(name[0])
+  {
+    case '-':
+    case '$':
+    case '%':
+    case '#':
+    case '>':
+    case ' ':
+      return 0;
+  }
+  // Cannot end with '- '
+  switch(name[name_len - 1])
+  {
+    case ' ':
+    case '-':
+      return 0;
+  }
+
+  // We still check the first and last char for the general rules
+  for(size_t i = 0; i < name_len; ++i)
+  {
+    char c = name[i];
+    // All names should be printable
+    if(!isprint(c))
+      return 0;
+    // Even within prints, there are more restrictions
+    switch(c)
+    {
+      case ':': // Used in a path to denote filesystem
+      case '/': // Used in a path to separate nodes
+      case ';': // Used to separate list of paths
+      case '~': // Ig i want to reserve this for a home-like directory
+      case '\\': // No confusion
+        return 0;
+    }
+  }
+
+  return 1;
+}
+
+// Warning; Functions assumes opath is at least as big as path
+// Also assumes path is valid even if non-canonical
+void fs_makecanonical(char *path, char *opath)
+{
+  size_t head = 0;
+  size_t ohead = 0;
+
+  while(isspace(path[head]))
+    ++head;
+
+  while(path[head] && path[head] != ':' && !isspace(path[head]))
+  {
+    opath[ohead] = path[head];
+    ++ohead;
+    ++head;
+  }
+
+  // Skip all whitespaces after filesystem name
+  while(isspace(path[head]))
+    ++head;
+
+  // Now path[head] = ':'
+  opath[ohead++] = ':';
+  opath[ohead++] = '/';
+  opath[ohead++] = '/';
+  head += 3;
+
+  // Now path[head] is the second / of the path
+  while(path[head])
+  {
+    // skip all whitespaces
+    while(isspace(path[head]))
+      ++head;
+
+    // If we find a '/' after the whitespace, then add a '/' to output, and skip
+    // all following '/' and spaces until first word or end of path
+    if(path[head] == '/')
+    {
+      if(opath[ohead-1] != '/')
+      {
+        opath[ohead] = '/';
+        ++ohead;
+      }
+      ++head;
+      while(path[head] == '/' || isspace(path[head]))
+        ++head;
+    }
+
+    size_t wb_head = head;
+    size_t wb_ohead = ohead;
+    while(path[head] && path[head] != '/')
+    {
+      opath[ohead] = path[head];
+      ++ohead;
+      ++head;
+    }
+    // track back the last sequence of spaces
+    while(isspace(opath[ohead-1]))
+      --ohead;
+
+    // If this was a '..', then we roll back to
+    // just after the second previous '/'
+    if(head - wb_head == 2 && path[wb_head] == '.' && path[wb_head] == '.')
+    {
+      ohead -= 4;
+      while(opath[ohead-1] != '/')
+        --ohead;
+    }
+
+    // If this was a '.'
+    if(head - wb_head == 1 && path[wb_head] == '.')
+      ohead = wb_ohead;
+  }
+  opath[ohead] = 0;
+}
+
+// Assumes path is valid
+// *nodes needs to be free()d later
+void fs_pathtok(char *path, char *fsname, char **nodes, size_t *len)
+{
+  size_t path_len = strlen(path);
+  char canon_path[path_len+1];
+
+  fs_makecanonical(path, canon_path);
+
+  size_t canon_path_len = strlen(canon_path);
+  *len = 0;
+  for(size_t i = 0; i < canon_path_len; ++i)
+  {
+    if(canon_path[i] == '/')
+      ++*len;
+  }
+
+  --*len;
+  if(canon_path[canon_path_len-1] == '/')
+    --*len;
+
+  size_t head = 0;
+  while(canon_path[head] != ':')
+  {
+    fsname[head] = canon_path[head];
+    ++head;
+  }
+
+  head += 3;
+
+  char *nbuf = calloc(*len, FSNODE_NAMELEN);
+  if(!nbuf)
+  {
+    *len = 0;
+    return;
+  }
+
+  *nodes = nbuf;
+
+  for(size_t i = 0; i < *len; ++i)
+  {
+    size_t ohead = i * FSNODE_NAMELEN;
+    while(canon_path[head] && canon_path[head] != '/')
+    {
+      nbuf[ohead] = canon_path[head];
+      ++ohead;
+      ++head;
+    }
+    nbuf[ohead] = 0;
+    ++ohead;
+    ++head;
+  }
+}

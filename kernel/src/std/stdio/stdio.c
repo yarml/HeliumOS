2746014787f6a2fd9094a4954fe4b6587f1d6bbd
yarml <youssefharmal@gmail.com>
2023-04-07 12:57:44 +0000

    Remove unnecessary printing of stdout & stderr redirection

--- kernel/src/std/stdio/stdio.c
+++ kernel/src/std/stdio/stdio.c
@@ -6,9 +6,7 @@ static FILE *s_stderr = 0;
 void __init_stdio()
 {
   s_stdout = fopen("debug://stdout", "a");
-  printf("stdout redirected to debug://stdout\n");
   s_stderr = fopen("debug://stderr", "a");
-  printf("stderr redirected to debug://stderr\n");
 }
 
 FILE *__get_stdout()

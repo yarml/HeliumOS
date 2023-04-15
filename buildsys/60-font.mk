22c4a1cf56d5cb6281d1e1bde9ac887b707aceba
yarml <youssefharmal@gmail.com>
2023-04-15 14:05:33 +0000

    Add font to initrd

---
+++ buildsys/60-font.mk
@@ -0,0 +1,15 @@
+FONT_NAME := tamsyn-font-1.11
+FONT_LINK := http://www.fial.com/~scott/tamsyn-font/download/$(FONT_NAME).tar.gz
+FONT_TYPE := Tamsyn8x15r
+
+FONT_FILE := $(INITRD_SYSROOT)/sys/font.psf
+
+$(FONT_FILE):
+	$(CURL) -o $(BUILD_DIR)/font.tar.gz $(FONT_LINK)
+	$(CD) $(BUILD_DIR) && $(TAR) -xvf $(BUILD_DIR)/font.tar.gz
+	$(CD) $(BUILD_DIR)/$(FONT_NAME)/ && $(GZIP) -d $(FONT_TYPE).psf.gz
+	$(MKDIR) -p $(dir $@)
+	$(MV) $(BUILD_DIR)/$(FONT_NAME)/$(FONT_TYPE).psf $@
+
+.PHONY: font
+font: $(FONT_FILE)

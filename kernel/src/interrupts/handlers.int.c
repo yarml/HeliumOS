c3af5bd67dc7342fcf30a3357372cb1a68dcee89
yarml <youssefharmal@gmail.com>
2023-04-17 20:10:20 +0000

    Fix exception_page_fault not having attribute interrupt_handler

--- kernel/src/interrupts/handlers.int.c
+++ kernel/src/interrupts/handlers.int.c
@@ -1,19 +1,21 @@
 #include <interrupts.h>
 #include <stdio.h>
+#include <term.h>
 #include <sys.h>
 
 #include <asm/ctlr.h>
 
 static void exception_common_prologue(int_frame *frame, char *name)
 {
-  printd("[EXCEPTION:%s]\n");
+  term_setfg(255, 0, 0);
+  fprintf(stderr, "[EXCEPTION:%s]\n", name);
   fprintf(
     stderr,
-    "IP: %#016lx\n"
-    "CS: %#016lx\n"
-    "RF: %#016lx\n"
-    "SP: %#016lx\n"
-    "SS: %#016lx\n",
+    "IP: %016lx\n"
+    "CS: %016lx\n"
+    "RF: %016lx\n"
+    "SP: %016lx\n"
+    "SS: %016lx\n",
     frame->ip,
     frame->cs,
     frame->flags,
@@ -22,21 +24,23 @@ static void exception_common_prologue(int_frame *frame, char *name)
   );
 }
 
-interrupt_handler void exception_div(int_frame* frame)
+interrupt_handler void exception_div(int_frame *frame)
 {
   exception_common_prologue(frame, "DIV ERROR");
 
   stop();
 }
 
-void exception_page_fault(int_frame *frame, int_errcode_pf err_code)
+interrupt_handler void exception_page_fault(int_frame *frame, uint64_t ec)
 {
+  int_errcode_pf *err_code = (void *) &ec;
+
   uint64_t adr = as_rcr2();
 
   exception_common_prologue(frame, "PAGE FAULT");
 
-  char *operation = err_code.write ? "write" : "read";
-  char *priv = err_code.user ? "user" : "kernel";
+  char *operation = err_code->write ? "write" : "read";
+  char *priv = err_code->user ? "user" : "kernel";
 
   fprintf(
     stderr,
@@ -46,28 +50,28 @@ void exception_page_fault(int_frame *frame, int_errcode_pf err_code)
     operation, priv, adr
   );
 
-  if(!err_code.present)
+  if(!err_code->present)
     fprintf(stderr, "Caused by structure without present flag.\n");
-  else if(err_code.rsvd)
+  else if(err_code->rsvd)
     fprintf(stderr, "Caused by reserved bit set to 1.\n");
   else
     fprintf(stderr, "Caused by page level protection.\n");
 
-  if(err_code.pk_violation)
+  if(err_code->pk_violation)
     fprintf(stderr, "Caused by protection-key violation.\n");
 
-  if(err_code.shadow_stack)
+  if(err_code->shadow_stack)
     fprintf(stderr, "Caused by shadow stack.\n");
 
-  if(err_code.ins_fetch)
-    fprintf(stderr, " While trying to fetch instruction.\n");
+  if(err_code->ins_fetch)
+    fprintf(stderr, "While trying to fetch instruction.\n");
 
-  if(err_code.hlat)
+  if(err_code->hlat)
     fprintf(stderr, "HLAT.\n");
 
-  if(err_code.sgx)
+  if(err_code->sgx)
     fprintf(stderr, "SGX.\n");
 
-  // Fot the far far far far far far far future, implement swapping here
+  // For the far far far far far far far future, implement swapping here
   stop();
 }

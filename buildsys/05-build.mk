a9e0ad7e75aa54fcbc117cd263ef7d4ba670f0fe
yarml <youssefharmal@gmail.com>
2023-04-19 11:33:47 +0000

    Mark clean, clean-cache, clean-all as PHONY targets

--- buildsys/05-build.mk
+++ buildsys/05-build.mk
@@ -2,14 +2,15 @@
 $(BUILD_DIR):
 	$(MKDIR) -p $(BUILD_DIR)
 
+.PHONY: clean clean-cache clean-all
 
 clean:
 	$(RM) $(CLEAN)
 
-# Will remove EVERYTHING inluding gcc and binutils builds
-clean-all: clean gcc-rm binutils-rm mkbootimg-rm bootboot-rm
-
 clean-cache:
 	$(RM) $(CACHE_DIR)
+
+# Will remove EVERYTHING inluding gcc and binutils builds
+clean-all: clean gcc-rm binutils-rm mkbootimg-rm bootboot-rm clean-cache
 
 export CC = $(BUILD_CC)

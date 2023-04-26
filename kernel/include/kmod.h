0984510fe60b77d1417a0f4c4803c5033545ec4c
yarml <youssefharmal@gmail.com>
2023-04-26 12:52:15 +0100

    Basic module loader

--- kernel/include/kmod.h
+++ kernel/include/kmod.h
@@ -1,12 +1,29 @@
 #ifndef HELIUM_KMOD_H
 #define HELIUM_KMOD_H
 
+#include <mem.h>
+
 #define KMOD_NAMELEN (256)
 
 typedef struct KMOD kmod;
 struct KMOD
 {
   char name[KMOD_NAMELEN];
+  mem_vseg vseg;
+};
+
+#define CM_UNDEF (0)
+#define CM_MAP   (1)
+#define CM_ZMEM  (2)
+
+typedef struct ELF64_KMOD_LOADER_COMMAND elf64_kmod_loader_command;
+struct ELF64_KMOD_LOADER_COMMAND
+{
+  uint64_t command;
+  uint64_t foff;
+  uint64_t moff;
+  uint64_t size;
+  uint64_t flags;
 };
 
 
@@ -17,7 +34,7 @@ int ksym_loadf(char const *path);
 int ksym_loadb(void *ksymf);
 
 kmod *kmod_loadf(char const *path);
-kmod *kmod_loadb(void *kmodf);
+kmod *kmod_loadb(void *kmodf, char name[KMOD_NAMELEN]);
 void kmod_uload(kmod *mod);
 
 
--- kernel/src/kmod/loader.c
+++ kernel/src/kmod/loader.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
@@ -31,14 +32,65 @@ kmod *kmod_loadf(char const *path)
     }
     read += cr;
   }
+  kmod *mod = kmod_loadb(buf, f->name);
   fs_close(f);
-  return kmod_loadb(buf);
+  return mod;
 }
 
-kmod *kmod_loadb(void *kmodf)
+kmod *kmod_loadb(void *kmodf, char name[KMOD_NAMELEN])
 {
   tpd("### Loading kernel module ###\n");
 
+
+  elf64_header *eh = kmodf;
+  elf64_prog_header *ph = kmodf + eh->phoff;
+  elf64_kmod_loader_command *cmd = kmodf + ph->offset;
+  size_t cmd_count = ph->file_size / sizeof(elf64_kmod_loader_command);
+
+  tpd("ALLOC size=%lz\n", ph->mem_size);
+  mem_vseg kmod_vseg = mem_alloc_vblock(
+    ph->mem_size,
+    MAPF_W | MAPF_R | MAPF_X, // TODO: Give 0 permissions by default
+    KMOD_HEAP,
+    KMOD_HEAP_SIZE
+  );
+
+  if(kmod_vseg.error)
+    return 0;
+
+  kmod *mod = calloc(1, sizeof(kmod));
+
+  strcpy(mod->name, name);
+  mod->vseg = kmod_vseg;
+
+  void *base = kmod_vseg.ptr;
+
+  for(size_t i = 0; i < cmd_count; ++i)
+  {
+    switch(cmd[i].command)
+    {
+      case CM_UNDEF:
+        tpd("UNDEF #\n");
+        break;
+      case CM_MAP:
+        tpd(
+          "MAP foff=%p, moff=%p, size=%lu, flags=%lx\n",
+          cmd[i].foff, cmd[i].moff, cmd[i].size, cmd[i].flags
+        );
+        memcpy(base + cmd[i].moff, kmodf + cmd[i].foff, cmd[i].size);
+        break;
+      case CM_ZMEM:
+        tpd(
+          "ZMEM moff=%p, size=%lu, flags=%lx\n",
+          cmd[i].moff, cmd[i].size, cmd[i].flags
+        );
+        memset(base + cmd[i].moff, 0, cmd[i].size);
+        break;
+    }
+  }
+  tpd("ENTRYPOINT moff=%p\n", eh->entrypoint);
+  int (*mod_init)() = base + eh->entrypoint;
+  tpd("Return value: %d\n", mod_init());
   return 0;
 }
 

a86349c10c29e953a3ce8d48755e68cddf489a16
yarml <youssefharmal@gmail.com>
2023-05-15 19:23:02 +0100

    Implement as_outw, as_outsw, as_outd, as_outsd

--- kernel/include/asm/io.h
+++ kernel/include/asm/io.h
@@ -8,4 +8,10 @@
 void as_outb(uint16_t port, uint8_t data);
 void as_outsb(uint16_t port, uint8_t *data, size_t len);
 
+void as_outw(uint16_t port, uint16_t data);
+void as_outsw(uint16_t port, uint16_t *data, size_t len);
+
+void as_outd(uint16_t port, uint32_t data);
+void as_outsd(uint16_t port, uint32_t *data, size_t len);
+
 #endif
--- kernel/src/asm/io.asm
+++ kernel/src/asm/io.asm
@@ -1,6 +1,12 @@
 global as_outb
 global as_outsb
 
+global as_outw
+global as_outsw
+
+global as_outd
+global as_outsd
+
 section .text
 
 as_outb:
@@ -15,3 +21,30 @@ as_outsb:
   mov rdx, rdi
   rep outsb
   ret
+
+as_outw:
+  mov rdx, rdi
+  mov rax, rsi
+  out dx, ax
+  ret
+
+as_outsw:
+  cld
+  mov rcx, rdx
+  mov rdx, rdi
+  rep outsw
+  ret
+
+as_outd:
+  mov rdx, rdi
+  mov rax, rsi
+  out dx, eax
+  ret
+
+as_outsd:
+  cld
+  mov rcx, rdx
+  mov rdx, rdi
+  rep outsd
+  ret
+

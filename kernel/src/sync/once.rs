8435deb04b2d075e5579b0b64841e650b3cb0779
yarml <youssefharmal@gmail.com>
2024-11-10 01:52:47 +0100

    Improve safety doc for Once

--- kernel/src/sync/once.rs
+++ kernel/src/sync/once.rs
@@ -1,9 +1,9 @@
-use core::{cell::Cell, hint, mem::MaybeUninit, sync::atomic::Ordering};
+use core::{cell::UnsafeCell, hint, mem::MaybeUninit, sync::atomic::Ordering};
 use status::{AtomicStatus, Status};
 
 pub struct Once<T> {
   status: AtomicStatus,
-  data: Cell<MaybeUninit<T>>,
+  data: UnsafeCell<MaybeUninit<T>>,
 }
 
 unsafe impl<T: Send> Send for Once<T> {}
@@ -13,7 +13,7 @@ impl<T> Once<T> {
   pub const fn new() -> Self {
     Self {
       status: AtomicStatus::new(Status::Uninit),
-      data: Cell::new(MaybeUninit::uninit()),
+      data: UnsafeCell::new(MaybeUninit::uninit()),
     }
   }
 }
@@ -34,7 +34,12 @@ impl<T> Once<T> {
       .is_ok()
     {
       let data = data_fn();
-      self.data.set(MaybeUninit::new(data));
+      *unsafe {
+        // # Safety:
+        // UnsafeCell pointer guarentees most "Pointer to reference conversion"
+        // self.status() == Runnin ensures we are the only current reference
+        &mut *self.data.get()
+      } = MaybeUninit::new(data);
       self.status.store(Status::Init, Ordering::Release);
       true
     } else {
@@ -47,7 +52,7 @@ impl<T> Once<T> {
   pub fn get(&self) -> Option<&T> {
     if self.status() == Status::Init {
       Some(unsafe {
-        // Safety: self.status == Status::Init
+        // Safety: self.status() == Status::Init
         self.force_get()
       })
     } else {
@@ -60,7 +65,7 @@ impl<T> Once<T> {
       Status::Uninit => None,
       Status::Init => {
         Some(unsafe {
-          // Safety: self.status == Status::Init
+          // Safety: self.status() == Status::Init
           self.force_get()
         })
       }
@@ -73,7 +78,7 @@ impl<T> Once<T> {
       hint::spin_loop();
     }
     unsafe {
-      // Safety: self.status == Status::Init
+      // Safety: self.status() == Status::Init
       self.force_get()
     }
   }
@@ -84,9 +89,21 @@ impl<T> Once<T> {
     self.status.load(Ordering::Acquire)
   }
 
-  // Safety: self.status == Status::Init
+  /// # Safety
+  /// Requires self.status() == Status::Init
   unsafe fn force_get(&self) -> &T {
-    (&*self.data.as_ptr()).assume_init_ref()
+    debug_assert_eq!(self.status(), Status::Init);
+    let data = unsafe {
+      // # Safety
+      // Pointer from UnsafeCell guarentees most "convertible to a reference" conditions
+      // self.status() == Status::Init ensures no aliasing &mut exists
+      &*self.data.get()
+    };
+    unsafe {
+      // # Safety
+      // self.status() == Status::Init ensures the value is initialized
+      data.assume_init_ref()
+    }
   }
 }
 
@@ -97,21 +114,26 @@ mod status {
   };
 
   #[repr(u8)]
-  #[derive(PartialEq)]
+  #[derive(Debug, PartialEq)]
   pub enum Status {
     Uninit,
     Running,
     Init,
   }
 
+  #[derive(Debug)]
   pub struct AtomicStatus {
     status: AtomicU8,
   }
 
   impl Status {
-    // Safety: raw ∈ Status
+    /// # Safety
+    /// Requires raw ∈ Status
     unsafe fn new_unchecked(raw: u8) -> Self {
-      mem::transmute(raw)
+      unsafe {
+        // Safety: Ensured by caller
+        mem::transmute(raw)
+      }
     }
   }
 
